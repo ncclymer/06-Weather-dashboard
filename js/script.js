0317d0426fa791323c1809fea249f71f59279dc2
unknown <nick_clymer2018@yahoo.com>
2021-03-23 17:47:27 -0600

    replaced table layout with standard html div to fix fetch error. Added js functionality. Still getting 400 due to bad syntax

--- js/script.js
+++ js/script.js
@@ -1,34 +1,118 @@
-var tableBody = document.getElementById('repo-table');
-var fetchButton = document.getElementById('fetch-button');
+var resultTextEl = document.querySelector('#result-text');
+var resultContentEl = document.querySelector('#result-content');
+var searchFormEl = document.querySelector('#fetch-button');
 
-function getApi() {
-    // fetch request gets current weather information
-     var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=820c7f8138019a10c7af3d05720e11d9';
+function getParams() {
+  // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
+  var searchParamsArr = document.location.search.split('&');
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-            //Loop over the data to generate a table, each table row will have a link to the repo url
-            for (var i = 0; i < data.length; i++) {
-                // Creating elements, tablerow, tabledata, and anchor
-                var createTableRow = document.createElement('tr');
-                var tableData = document.createElement('td');
-                var link = document.createElement('a');
+  // Get the query and format values
+  var query = searchParamsArr[0].split('=').pop();
+//   var format = searchParamsArr[1].split('=').pop();
 
-                // Setting the text of link and the href of the link
-                link.textContent = data[i].html_url;
-                link.href = data[i].html_url;
-
-                // Appending the link to the tabledata and then appending the tabledata to the tablerow
-                // The tablerow then gets appended to the tablebody
-                tableData.appendChild(link);
-                createTableRow.appendChild(tableData);
-                tableBody.appendChild(createTableRow);
-            }
-        });
+//   add , format to the function below
+  searchApi(query);
 }
 
-fetchButton.addEventListener('click', getApi);
+function printResults(resultObj) {
+  console.log(resultObj);
+
+  // set up `<div>` to hold result content
+  var resultCard = document.createElement('div');
+  resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+
+  var resultBody = document.createElement('div');
+  resultBody.classList.add('card-body');
+  resultCard.append(resultBody);
+
+  var titleEl = document.createElement('h3');
+  titleEl.textContent = resultObj.title;
+
+  var bodyContentEl = document.createElement('p');
+  bodyContentEl.innerHTML =
+    '<strong>Date:</strong> ' + resultObj.date + '<br/>';
+
+  if (resultObj.subject) {
+    bodyContentEl.innerHTML +=
+      '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
+  } else {
+    bodyContentEl.innerHTML +=
+      '<strong>Subjects:</strong> No subject for this entry.';
+  }
+
+  if (resultObj.description) {
+    bodyContentEl.innerHTML +=
+      '<strong>Description:</strong> ' + resultObj.description[0];
+  } else {
+    bodyContentEl.innerHTML +=
+      '<strong>Description:</strong>  No description for this entry.';
+  }
+
+  var linkButtonEl = document.createElement('a');
+  linkButtonEl.textContent = 'Read More';
+  linkButtonEl.setAttribute('href', resultObj.url);
+  linkButtonEl.classList.add('btn', 'btn-dark');
+
+  resultBody.append(titleEl, bodyContentEl, linkButtonEl);
+
+  resultContentEl.append(resultCard);
+}
+// add , format to below function pass
+function searchApi(query) {
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather/?q=' + query + '&appid=820c7f8138019a10c7af3d05720e11d9';
+
+//   if (format) {
+//     requestUrl = 'https://www.api.openweathermap.org/data/2.5/weather' + format + '/?q=&appid=820c7f8138019a10c7af3d05720e11d9';
+  
+
+//   requestUrl = requestUrl + '&q=' + query;
+
+
+  fetch(requestUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+
+      return response.json();
+    })
+    .then(function (locRes) {
+      // write query to page so user knows what they are viewing
+      resultTextEl.textContent = locRes.search.query;
+
+      console.log(locRes);
+
+      if (!locRes.results.length) {
+        console.log('No results found!');
+        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+      } else {
+        resultContentEl.textContent = '';
+        for (var i = 0; i < locRes.results.length; i++) {
+          printResults(locRes.results[i]);
+        }
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+ }
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector('#search-input').value;
+//   var formatInputVal = document.querySelector('#format-input').value;
+
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
+// add , formatInputVal to the function below
+  searchApi(searchInputVal);
+}
+
+searchFormEl.addEventListener('click', handleSearchFormSubmit);
+
+getParams();
+
+

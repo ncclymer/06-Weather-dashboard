866b4ee80fd3fb1cb755bbad92e7cc757e79aeb6
unknown <nick_clymer2018@yahoo.com>
2021-03-24 21:31:21 -0600

    end of day save

--- js/script.js
+++ js/script.js
@@ -1,62 +1,70 @@
 // Api data URLs
-requestUrl = 'https://api.openweathermap.org/data/2.5/weather/?q=denver&units=imperial&appid=820c7f8138019a10c7af3d05720e11d9';
+
+// requestUrl = 'https://api.openweathermap.org/data/2.5/weather/?q=' + userInput + '&units=imperial&appid=820c7f8138019a10c7af3d05720e11d9';
+
 uvIndexUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=39.7392&lon=-104.9847&units=imperial&appid=820c7f8138019a10c7af3d05720e11d9';
+// uvIndexUrl = 'https://api.openweathermap.org/data/2.5/onecall?' + lat + '&' + lon + '&units=imperial&appid=820c7f8138019a10c7af3d05720e11d9';
+
 fiveDayUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=denver&units=imperial&appid=820c7f8138019a10c7af3d05720e11d9';
+// fiveDayUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + userInput + '&units=imperial&appid=820c7f8138019a10c7af3d05720e11d9';
+
+
 
 // function target button
 fetchButton = document.querySelector('#fetch-button');
 
-// function converts URL parameters into an array
-function convertParameters() {
-var paramArray = document.location.search.split('&');
-var query = paramArray[0].split('=').pop();
-searchApi(query);
-}
+// // function converts URL parameters into an array
+// function convertParameters() {
+//   var paramArray = document.location.search.split('&');
+//   var query = paramArray[0].split('=').pop();
+//   searchApi(query);
+// }
+
 
 // calls temp, humidity, and wind speed * need to write concat so that user data is entered into the ?q={X} portion of the query string.
 function getWeather(event) {
-    event.preventDefault();
+  event.preventDefault();
+  requestUrl = 'https://api.openweathermap.org/data/2.5/weather/?q=denver&units=imperial&appid=820c7f8138019a10c7af3d05720e11d9';
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            document.getElementById('temp').textContent = data.main.temp + ' °F';
-            document.getElementById('humidity').textContent = data.main.humidity + '%';
-            document.getElementById('wind-speed').textContent = data.wind.speed + ' MPH';
-            console.log(data)
-            // console.log(data.main.temp);
-            // console.log(data.main.humidity);
-            // console.log(data.wind.speed);
-        });
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      document.getElementById('city').textContent = 'Current conditions for: ' + data.name + data.weather[0].icon;
+      document.getElementById('temp').textContent = 'Temperature:' + data.main.temp + ' °F';
+      document.getElementById('humidity').textContent = 'Humidty:' +  data.main.humidity + '%';
+      document.getElementById('wind-speed').textContent = 'Wind Speed:' + data.wind.speed + ' MPH';
+      console.log(data)
+    });
 }
 
 // calls uvi index * need to pull lat and lon data from first array
 function getUvi(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    fetch(uvIndexUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            document.getElementById('UVindex').textContent = data.current.uvi;
-            console.log(data);
-        });
+  fetch(uvIndexUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      document.getElementById('UVindex').textContent = 'UV Index:' + data.current.uvi;
+      console.log(data);
+    });
 }
 
 // calls 5 day forcast
 function getFiveDay(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    fetch(fiveDayUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        });
+  fetch(fiveDayUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      document.getElementById('day1').textContent = data.list[2].dt_txt;
+      console.log(data);
+    });
 }
 
 // user click event for target button

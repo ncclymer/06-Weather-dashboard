bb096dcb68c937a6af523c0120740a31c140b20e
unknown <nick_clymer2018@yahoo.com>
2021-03-28 18:42:34 -0600

    duplicated js scripts for remaining forcast elements

--- js/script.js
+++ js/script.js
@@ -90,6 +90,42 @@ function getFiveDay(event) {
       document.getElementById('1temp').textContent = 'Temp: ' + data.list[2].main.temp + ' °F';
       document.getElementById('1humidity').textContent = 'Humidity: ' + data.list[2].main.humidity + ' %';
       document.getElementById('1wind-speed').textContent = 'Wind: ' + data.list[2].wind.speed + ' MPH';
+
+
+      day2Date = document.getElementById('2-date');
+      day2Icon = document.getElementById("2-icon");
+      day2Date.textContent = moment.unix(data.list[2].dt).format("MMM D, YYYY");
+      day2Icon.setAttribute("src", "http://openweathermap.org/img/w/" + data.list[10].weather[0].icon + ".png");
+      document.getElementById('2temp').textContent = 'Temp: ' + data.list[10].main.temp + ' °F';
+      document.getElementById('2humidity').textContent = 'Humidity: ' + data.list[10].main.humidity + ' %';
+      document.getElementById('2wind-speed').textContent = 'Wind: ' + data.list[10].wind.speed + ' MPH';
+
+
+      day3Date = document.getElementById('3-date');
+      day3Icon = document.getElementById("3-icon");
+      day3Date.textContent = moment.unix(data.list[2].dt).format("MMM D, YYYY");
+      day3Icon.setAttribute("src", "http://openweathermap.org/img/w/" + data.list[18].weather[0].icon + ".png");
+      document.getElementById('3temp').textContent = 'Temp: ' + data.list[18].main.temp + ' °F';
+      document.getElementById('3humidity').textContent = 'Humidity: ' + data.list[18].main.humidity + ' %';
+      document.getElementById('3wind-speed').textContent = 'Wind: ' + data.list[18].wind.speed + ' MPH';
+
+
+      day4Date = document.getElementById('4-date');
+      day4Icon = document.getElementById("4-icon");
+      day4Date.textContent = moment.unix(data.list[2].dt).format("MMM D, YYYY");
+      day4Icon.setAttribute("src", "http://openweathermap.org/img/w/" + data.list[26].weather[0].icon + ".png");
+      document.getElementById('4temp').textContent = 'Temp: ' + data.list[26].main.temp + ' °F';
+      document.getElementById('4humidity').textContent = 'Humidity: ' + data.list[26].main.humidity + ' %';
+      document.getElementById('4wind-speed').textContent = 'Wind: ' + data.list[26].wind.speed + ' MPH';
+
+
+      day5Date = document.getElementById('5-date');
+      day5Icon = document.getElementById("5-icon");
+      day5Date.textContent = moment.unix(data.list[2].dt).format("MMM D, YYYY");
+      day5Icon.setAttribute("src", "http://openweathermap.org/img/w/" + data.list[34].weather[0].icon + ".png");
+      document.getElementById('5temp').textContent = 'Temp: ' + data.list[34].main.temp + ' °F';
+      document.getElementById('5humidity').textContent = 'Humidity: ' + data.list[34].main.humidity + ' %';
+      document.getElementById('5wind-speed').textContent = 'Wind: ' + data.list[34].wind.speed + ' MPH';
       console.log(data);
     });
 }
